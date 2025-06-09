e2927c3084126baf14e883c2fdba2a16a2576275
RiteshGupta19 <riteshgupta0968@gmail.com>
2025-06-09 15:33:20 +0000

    minikube changes

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,13 @@ app.use(express.json());  // To parse incoming JSON requests
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['https://taskmaster-io.netlify.app', 'http://localhost:5173'],
+    origin: ['https://taskmaster-io.netlify.app','http://localhost:5173','http://localhost:3000','http://192.168.49.2:30000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }));
 
-
+app.get('/health', (req, res) => res.status(200).send('OK'));
+app.get('/ready', (req, res) => res.status(200).send('READY'));
 // app.use(cors());
 
 app.use('/', Router);
